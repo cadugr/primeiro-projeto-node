3216f957b512673bae6eb4461e5085890ef16ab6
Carlos Eduardo <cadu.guerra@gmail.com>
2020-05-11 12:25:08 -0300

    Removendo import não mais utilizado

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,4 +1,4 @@
-import { Router, Response } from 'express';
+import { Router } from 'express';
 import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
